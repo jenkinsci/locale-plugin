41dc8d4f93770b2147b38afd196d192fb16bb8e2
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-05-25 15:57:31 +0200

    Support user language preference (#306)
    * Support user language preference (JENKINS-57456)
    
    * Add @RequirePOST where needed

--- src/main/java/hudson/plugins/locale/LocaleFilter.java
+++ src/main/java/hudson/plugins/locale/LocaleFilter.java
@@ -1,5 +1,8 @@
 package hudson.plugins.locale;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import hudson.model.User;
+import hudson.plugins.locale.user.UserLocaleProperty;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -8,6 +11,7 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Locale;
@@ -19,27 +23,40 @@ import java.util.NoSuchElementException;
 public class LocaleFilter implements Filter {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
         // nop
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        if (request instanceof HttpServletRequest) {
+        if (request instanceof HttpServletRequest req) {
             PluginImpl plugin = PluginImpl.get();
-            if (plugin != null && plugin.isIgnoreAcceptLanguage()) {
-                request = new HttpServletRequestWrapper((HttpServletRequest) request) {
+            final Locale locale;
+
+            if (plugin == null) {
+                chain.doFilter(request, response);
+                return;
+            } else if (plugin.isAllowUserPreferences()) {
+                locale = getCurrentUserLocale();
+            } else if (plugin.isIgnoreAcceptLanguage()) {
+                locale = Locale.getDefault();
+            } else {
+                locale = null;
+            }
+
+            if (locale != null) {
+                request = new HttpServletRequestWrapper(req) {
                     @Override
                     public Locale getLocale() {
-                        // Force locale to configured default, ignore request' Accept-Language header
-                        return Locale.getDefault();
+                        // Force locale to configured default, ignore requests Accept-Language header
+                        return locale;
                     }
 
                     @Override
                     public Enumeration<Locale> getLocales() {
                         // Create a custom Enumeration with only the default locale
-                        return new Enumeration<Locale>() {
+                        return new Enumeration<>() {
                             private boolean hasMoreElements = true;
 
                             @Override
@@ -59,6 +76,7 @@ public class LocaleFilter implements Filter {
                         };
                     }
                 };
+                ((HttpServletResponse) response).addHeader("X-Jenkins-Language", locale.toString());
             }
         }
         chain.doFilter(request, response);
@@ -67,5 +85,18 @@ public class LocaleFilter implements Filter {
     @Override
     public void destroy() {
         // nop
+    }
+
+    @CheckForNull
+    private Locale getCurrentUserLocale() {
+        User user = User.current();
+        if (user != null) {
+            UserLocaleProperty userLocaleProperty = user.getProperty(UserLocaleProperty.class);
+            if (userLocaleProperty == null) {
+                return null;
+            }
+            return userLocaleProperty.getLocale();
+        }
+        return null;
     }
 }
